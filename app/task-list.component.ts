4a215151aa3f49cd3a5496c8823e8577e088f255
Jon Nardine <student@epicodus.com>
2016-03-22 12:01:12 -0700

    lunch, halfway through binding lesson

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,13 +1,21 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
+import { EditTaskDetailsComponent} from './edit-task-details.component';c
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  directives: [TaskComponent],
-  templateUrl: 'app/task-list.component.html'
+  directives: [TaskComponent, EditTaskDetailsComponent],
+  templateUrl: `
+    <task-display *ngFor="#currentTask of taskList"
+      (click)="taskClicked(currentTask)"
+      [class.selected]="currentTask === selectedTask"
+      [task]="currentTask">
+    </task-display>
+  <edit-task-details [task]="selectedTask">
+  </edit-task-details>`
 })
 export class TaskListComponent {
   public taskList: Task[];
