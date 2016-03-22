6f03c32e79fea5d8a9080135ffc2e3b78d2c587c
Jon Nardine <student@epicodus.com>
2016-03-22 09:25:36 -0700

    add guipfile and resources directory with simple style

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <div class="container">
+      <h1>Skeleton Angular2 App!</h1>
+    </div>
   `
 })
-
 export class AppComponent {
 
 }
